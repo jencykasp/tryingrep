d0ca75f6f6496d1b0930051ab95ff35bb5a23b40
jencykasp <jency.kasp@gmail.com>
2022-10-11 12:14:31 +0530

    comiting hello

---
+++ LearnJava2/src/create/helloworld.java
@@ -0,0 +1,9 @@
+package create;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world !   "+" Rihan "+" Ria " );	}
+
+}
